ac8f2056a19216cba2ac91821a107d29d281a1be
Fachi <farairubvuta@gmail.com>
2022-02-01 22:48:14 +0200

    tweaked tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   content: [
-    "bulid/**/*.{js,ts,jsx,tsx}",
+    "_next/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
