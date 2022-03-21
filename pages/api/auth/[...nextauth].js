3045f64b8085768b02886d1973be63e13c49359f
Fachi-r <farairubvuta@gmail.com>
2022-03-21 12:06:28 +0200

    removed public credentials

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -5,18 +5,6 @@ import EmailProvider from "next-auth/providers/email";
 import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 import clientPromise from "../../../mongodb_d";
 
-const GOOGLE_CLIENT_ID1 = '906119606745-67am70lhsr6b5p1vf3thjj5k16a2fdl2.apps.googleusercontent.com'
-const GOOGLE_CLIENT_SECRET1 = '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'
-const GOOGLE_CLIENT_ID = '662922025786-5er61ddsp2ndc6iel83oegcmathoahuo.apps.googleusercontent.com'
-const GOOGLE_CLIENT_SECRET = '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'
-const EMAIL_SERVER_USER = 'apikey'
-const EMAIL_SERVER_PASSWORD = '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'
-const EMAIL_SERVER_HOST = 'smtp.sendgrid.net'
-const EMAIL_SERVER_PORT = '465'
-const EMAIL_FROM = 'ticketxpress3@gmail.com'
-const GITHUB_ID = '47e410f2279a7ccdf760'
-const GITHUB_SECRET = '59c69918ff3329b826a4aaf61ed051597f92de7c'
-
 const options = {
   adapter: MongoDBAdapter(clientPromise),
   // Configure one or more authentication providers
